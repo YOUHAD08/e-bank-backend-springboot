076448c847512e8945d84c9ace1cdb503ddf88ca
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-07-08 22:07:53 +0100

    Backend : Map the relationship between Customer and BankAccount and AccountOperation using JPA annotations

--- src/main/java/ma/youhad/backend/entities/AccountOperation.java
+++ src/main/java/ma/youhad/backend/entities/AccountOperation.java
@@ -1,5 +1,6 @@
 package ma.youhad.backend.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,18 @@ import ma.youhad.backend.enums.OperationType;
 
 import java.util.Date;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountOperation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date operationDate;
     private double amount;
     private OperationType type;
+    @ManyToOne()
     private BankAccount bankAccount;
 }
--- src/main/java/ma/youhad/backend/entities/BankAccount.java
+++ src/main/java/ma/youhad/backend/entities/BankAccount.java
@@ -1,5 +1,9 @@
 package ma.youhad.backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +12,18 @@ import ma.youhad.backend.enums.AccountStatus;
 import java.util.Date;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class BankAccount {
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public abstract class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date createdAt;
     private AccountStatus status;
+
+    @ManyToOne
     private Customer customer;
+
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperation> accountOperations;
 }
